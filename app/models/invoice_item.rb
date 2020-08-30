024a4eb00526b488a41a74c520770984eb62c37e
Chandler Hulstrom <chulstro11@gmail.com>
2020-08-30 12:28:32 -0600

    Flushed out model validations and relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,5 @@
 class InvoiceItem < ApplicationRecord
+  validates_presences_of :quantity, :unit_price
   belongs_to :item
   belongs_to :invoice
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
+  validates_presences_of :credit_card_number, :result
   belongs_to :invoice
 end
